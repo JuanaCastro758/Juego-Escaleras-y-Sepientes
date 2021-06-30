ffe94ab7b4a9dc31e4d85571b0156c8348c748b0
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-30 14:54:06 -0600

    Se le coloco el nombre a los botones

--- src/formularios/JuegoPrincipal.java
+++ src/formularios/JuegoPrincipal.java
@@ -12,32 +12,37 @@ package formularios;
  * @author Chiko malo
  */
 
+import leerArchivo.LeerArchivo;
 import java.awt.Color;
 import java.io.File;
+import static java.lang.Math.random;
+import java.util.Random;
 import javax.swing.JButton;
-import jugador.Jugador;
-import leerArchivo.LeerArchivo;
+import javax.swing.JOptionPane;
 public class JuegoPrincipal extends javax.swing.JFrame {
 
     /**
      * Creates new form JuegoPrincipal
      */
-    private Jugador jugador=new Jugador();
     private int FILS=8;
     private int COLS=8;
     JButton[][] CUADRO;
-    JButton[][] jugador1;
     String[][] juego=new String[8][8];
-    
+    private int contadorf;
+    private int contadorc;
+    private int contadorf2;
+    private int contadorc2;
     public JuegoPrincipal() {
         initComponents();
         this.setLocationRelativeTo(null);
-        txtjugador1.setText("Turno del primer Jugador");
-        txtjugador2.setText("Turno del segundo Jugador");
         setLeer();
         setMatriz();
+        contadorf=0;
+        contadorc=0;
+        contadorf2=0;
+        contadorc2=0;
     }
-    private void setLeer(){
+    public void setLeer(){
         File archivo =new File ("posicion.txt");
         LeerArchivo nc=new LeerArchivo();
         String[] arreglo=nc.leer(archivo);
@@ -52,68 +57,70 @@ public class JuegoPrincipal extends javax.swing.JFrame {
             }
         }
     }
-    private void setMatriz(){
+    public void setMatriz(){
         CUADRO=new JButton[FILS][COLS];
-        int x=10;
-        int y=10;
+        int x=15;
+        int y=15;
         for(int i=0;i<FILS;i++){
             for(int j=0;j<COLS;j++){
                 if(juego[i][j].equals("fin")){
                     boton(CUADRO,i,j,x,y,Color.red);
                     CUADRO[i][j].setName("fin");
+                    CUADRO[i][j].setText("fin");
                 }else if(juego[i][j].equals("libre")){
                     boton(CUADRO,i,j,x,y,Color.green );
                     CUADRO[i][j].setName("libre");
+                    CUADRO[i][j].setText("libre");
                 }else if(juego[i][j].equals("tiro")){
                     boton(CUADRO,i,j,x,y,Color.white);
                     CUADRO[i][j].setName("tiro");
+                    CUADRO[i][j].setText("dadoGanador");
                 }else if(juego[i][j].equals("pierde")){
                     boton(CUADRO,i,j,x,y,Color.black);
                     CUADRO[i][j].setName("pierde");
-                }else if(juego[i][j].equals("pierde")){
-                    boton(CUADRO,i,j,x,y,Color.black);
+                    CUADRO[i][j].setText("paroTurno");
                 }else if(juego[i][j].equals("s4inicio")||juego[i][j].equals("s3inicio")||juego[i][j].equals("s2inicio")||juego[i][j].equals("s1inicio")||juego[i][j].equals("s4fin")||juego[i][j].equals("s3fin")||juego[i][j].equals("s2fin")||juego[i][j].equals("s1fin")){
                     boton(CUADRO,i,j,x,y,Color.blue);
                     //inicio de la cabeza
-                    if(juego[i][j].equals("s4inicio")){CUADRO[i][j].setName("s4inicio");}
-                    if(juego[i][j].equals("s3inicio")){CUADRO[i][j].setName("s3inicio");}
-                    if(juego[i][j].equals("s2inicio")){CUADRO[i][j].setName("s2inicio");}
-                    if(juego[i][j].equals("s1inicio")){CUADRO[i][j].setName("s1inicio");}
+                    if(juego[i][j].equals("s4inicio")){CUADRO[i][j].setName("s4inicio"); CUADRO[i][j].setText("4colaSer");}
+                    if(juego[i][j].equals("s3inicio")){CUADRO[i][j].setName("s3inicio"); CUADRO[i][j].setText("3colaSer");}
+                    if(juego[i][j].equals("s2inicio")){CUADRO[i][j].setName("s2inicio"); CUADRO[i][j].setText("2colaSer");}
+                    if(juego[i][j].equals("s1inicio")){CUADRO[i][j].setName("s1inicio"); CUADRO[i][j].setText("1colaSer");}
                     //fin de la cola
-                    if(juego[i][j].equals("s4fin")){CUADRO[i][j].setName("s4fin");}
-                    if(juego[i][j].equals("s3fin")){CUADRO[i][j].setName("s3fin");}
-                    if(juego[i][j].equals("s2fin")){CUADRO[i][j].setName("s2fin");}
-                    if(juego[i][j].equals("s1fin")){CUADRO[i][j].setName("s1fin");}
+                    if(juego[i][j].equals("s4fin")){CUADRO[i][j].setName("s4fin"); CUADRO[i][j].setText("4cabeSe");}
+                    if(juego[i][j].equals("s3fin")){CUADRO[i][j].setName("s3fin"); CUADRO[i][j].setText("3cabeSe");}
+                    if(juego[i][j].equals("s2fin")){CUADRO[i][j].setName("s2fin"); CUADRO[i][j].setText("2cabeSe");}
+                    if(juego[i][j].equals("s1fin")){CUADRO[i][j].setName("s1fin"); CUADRO[i][j].setText("1cabeSe");}
                 }else if(juego[i][j].equals("inicio")){
                     boton(CUADRO,i,j,x,y,Color.yellow );
                     CUADRO[i][j].setName("inicio");
+                    CUADRO[i][j].setText("Inicio");
                 }else{
                     boton(CUADRO,i,j,x,y,Color.cyan );
                     //inicio de la cabeza
-                    if(juego[i][j].equals("e4inicio")){CUADRO[i][j].setName("e4inicio");}
-                    if(juego[i][j].equals("e3inicio")){CUADRO[i][j].setName("e3inicio");}
-                    if(juego[i][j].equals("e2inicio")){CUADRO[i][j].setName("e2inicio");}
-                    if(juego[i][j].equals("e1inicio")){CUADRO[i][j].setName("e1inicio");}
+                    if(juego[i][j].equals("e4inicio")){CUADRO[i][j].setName("e4inicio"); CUADRO[i][j].setText("4EscFin");}
+                    if(juego[i][j].equals("e3inicio")){CUADRO[i][j].setName("e3inicio"); CUADRO[i][j].setText("3EscFin");}
+                    if(juego[i][j].equals("e2inicio")){CUADRO[i][j].setName("e2inicio"); CUADRO[i][j].setText("2EscFin");}
+                    if(juego[i][j].equals("e1inicio")){CUADRO[i][j].setName("e1inicio"); CUADRO[i][j].setText("1EscFin");}
                     //fin de la cola
-                    if(juego[i][j].equals("e4fin")){CUADRO[i][j].setName("e4fin");}
-                    if(juego[i][j].equals("e3fin")){CUADRO[i][j].setName("e3fin");}
-                    if(juego[i][j].equals("e2fin")){CUADRO[i][j].setName("e2fin");}
-                    if(juego[i][j].equals("e1fin")){CUADRO[i][j].setName("e1fin");}
+                    if(juego[i][j].equals("e4fin")){CUADRO[i][j].setName("e4fin"); CUADRO[i][j].setText("4EscaIn");}
+                    if(juego[i][j].equals("e3fin")){CUADRO[i][j].setName("e3fin"); CUADRO[i][j].setText("3EscaIn");}
+                    if(juego[i][j].equals("e2fin")){CUADRO[i][j].setName("e2fin"); CUADRO[i][j].setText("2EscaIn");}
+                    if(juego[i][j].equals("e1fin")){CUADRO[i][j].setName("e1fin"); CUADRO[i][j].setText("1EscaIn");}
                 }
-                x+=40;
+                x+=80;
             }
-            x=10;
-            y+=40;
+            x=15;
+            y+=80;
         }
     }
-    private void boton(JButton[][] C,int i,int j,int x,int y, Color c){
+    public void boton(JButton[][] C,int i,int j,int x,int y, Color c){
         C[i][j]=new JButton();
         C[i][j].setBackground(c);
-        C[i][j].setBounds(x, y, 40, 40);
+        C[i][j].setBounds(x, y, 75, 75);
                 
-        jPanel1.add(C[i][j]);
+        panelBotones.add(C[i][j]);
     }
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -123,160 +130,87 @@ public class JuegoPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jToolBar1 = new javax.swing.JToolBar();
-        btnDado2 = new javax.swing.JButton();
-        txtResultado2 = new javax.swing.JTextField();
         txtTurno = new javax.swing.JLabel();
-        txtjugador2 = new javax.swing.JTextField();
-        btnDado1 = new javax.swing.JButton();
-        txtResultado1 = new javax.swing.JTextField();
-        txtjugador1 = new javax.swing.JTextField();
+        panelBotones = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        dos = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        dos1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 153, 51));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jToolBar1.setRollover(true);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(168, Short.MAX_VALUE)
-                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(142, 142, 142))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(139, 139, 139)
-                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(306, Short.MAX_VALUE))
-        );
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 10, 410, 470));
-
-        btnDado2.setBackground(new java.awt.Color(204, 0, 0));
-        btnDado2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        btnDado2.setForeground(new java.awt.Color(255, 255, 255));
-        btnDado2.setText("Tirar Dado");
-        btnDado2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDado2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btnDado2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 250, 110, 50));
-
-        txtResultado2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        getContentPane().add(txtResultado2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, 110, -1));
-
         txtTurno.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         getContentPane().add(txtTurno, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 30, -1, -1));
-        getContentPane().add(txtjugador2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 220, 110, -1));
 
-        btnDado1.setBackground(new java.awt.Color(204, 0, 0));
-        btnDado1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        btnDado1.setForeground(new java.awt.Color(255, 255, 255));
-        btnDado1.setText("Tirar Dado");
-        btnDado1.addActionListener(new java.awt.event.ActionListener() {
+        javax.swing.GroupLayout panelBotonesLayout = new javax.swing.GroupLayout(panelBotones);
+        panelBotones.setLayout(panelBotonesLayout);
+        panelBotonesLayout.setHorizontalGroup(
+            panelBotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 730, Short.MAX_VALUE)
+        );
+        panelBotonesLayout.setVerticalGroup(
+            panelBotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 690, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(panelBotones, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 0, 730, 690));
+
+        jButton1.setText("jugador uno");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDado1ActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(btnDado1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, 110, 50));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 130, 50));
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, 150, -1));
+        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 50, 110, -1));
 
-        txtResultado1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
-        getContentPane().add(txtResultado1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 180, 110, -1));
-
-        txtjugador1.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setText("Jugador dos");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtjugador1ActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(txtjugador1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 110, -1));
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, 130, 60));
+        getContentPane().add(dos, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 410, 150, 30));
+
+        jLabel1.setText("Posicion del Jugador 2: ");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 380, -1, -1));
+
+        jLabel2.setText("Posicion del Jugador 1: ");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 320, -1, -1));
+        getContentPane().add(dos1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 340, 150, 30));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+         
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+       
     
-    private void btnDado2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDado2ActionPerformed
-        
-        
-    }//GEN-LAST:event_btnDado2ActionPerformed
-    int fi=7;
-    int colu=7;
-    int s=0;
-    private void btnDado1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDado1ActionPerformed
-       try{
-        setMatriz();
-        int x=jugador.getDado(1, 6);
-        txtResultado1.setText(" "+x);
-        int pos=0;
-        for(int i=fi;i>=0;i--){
-            if(i==7){
-                for(int j=colu;j>=0;j--){
-                    if(pos==x){
-                            CUADRO[i][j].setBackground(Color.PINK);
-                    }        
-                    pos++;
-                }
-            }
-            
-        }
-       }catch(Exception e){
-           System.out.println(e.getMessage()+" btnDado1 JuegoPrincipal");
-       }
-    }//GEN-LAST:event_btnDado1ActionPerformed
-
-    private void txtjugador1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtjugador1ActionPerformed
-        
-    }//GEN-LAST:event_txtjugador1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JuegoPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JuegoPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JuegoPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JuegoPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new JuegoPrincipal().setVisible(true);
-            }
-        });
-    }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnDado1;
-    private javax.swing.JButton btnDado2;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JToolBar jToolBar1;
-    private javax.swing.JTextField txtResultado1;
-    private javax.swing.JTextField txtResultado2;
+    private javax.swing.JTextField dos;
+    private javax.swing.JTextField dos1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JPanel panelBotones;
     private javax.swing.JLabel txtTurno;
-    private javax.swing.JTextField txtjugador1;
-    private javax.swing.JTextField txtjugador2;
     // End of variables declaration//GEN-END:variables
 }
