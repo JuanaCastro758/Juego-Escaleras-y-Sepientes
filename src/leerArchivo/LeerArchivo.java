551a4429934c98c927b9034b28ab4e29f07ceff6
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-29 20:10:18 -0600

    lee el archivo y lo retorna como una matriz unidimensional

---
+++ src/leerArchivo/LeerArchivo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leerArchivo;
+
+/**
+ *
+ * @author Chiko malo
+ */
+import java.io.*;
+public class LeerArchivo {
+        public String[] leer(File archivo){
+            int lineasTotales=0;
+        try{
+            if(archivo.exists()){
+                BufferedReader leerArchivo=new BufferedReader(new FileReader(archivo));
+                String lineaLeida="";
+                while((lineaLeida = leerArchivo.readLine())!=null){
+                    String[] palabras=lineaLeida.split(" ");
+                    return palabras;
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage()+" Error");  
+        }
+        return null;
+    }
+}
+
+
